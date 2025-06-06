91b7d653801dcd4fccb0341e91b5533642190b7e
LittleRaisin <h_kiyomiya@msn.com>
2025-06-06 23:06:37 +0900

    コピーボタンのアイコン化とトースト実装

--- src/ui/components/DetailPanel.tsx
+++ src/ui/components/DetailPanel.tsx
@@ -1,4 +1,4 @@
-import Button from './Button';
+import { Toaster, toast } from 'react-hot-toast';
 
 type Translation = {
   danbooru_name: string;
@@ -10,32 +10,54 @@ type Translation = {
   updated_at?: string;
 };
 
+type Props = {
+  label: string;
+  value?: string;
+  onCopy: (value?: string) => void;
+};
+
+const TitleWithCopy = ({ label, value, onCopy }: Props) => (
+  <div className='flex gap-2 items-center mb-1'>
+    <p className='font-bold'>{label}</p>
+    <img
+      src="/ico_copy.svg"
+      alt="copy"
+      className='inline-block w-4 h-4 cursor-pointer'
+      title="コピー"
+      onClick={() => onCopy(value)}
+    />
+  </div>
+);
+
 const DetailPanel = ({ item }: { item: Translation | null }) => {
   if (!item) return null;
 
-  const handleCopy = () => {
-    if (item.note) {
-      navigator.clipboard.writeText(item.note);
+  const handleCopy = (target :string | undefined) => {
+    if (target) {
+      navigator.clipboard.writeText(target).then(() => {
+        toast.success("コピーしました!");
+      })
     }
   };
 
   return (
     <div>
+      <Toaster position="top-right" />
       <div>
         {item.translation_text}
         <span className='inline-block ml-2'>{item.favorite ? "★" : "☆"}</span>
       </div>
       <div className='mt-3'>
-        <p>Danbooruタグ名</p>
-        <p>{item.danbooru_name}</p>
+        <TitleWithCopy label="Danbooruタグ名" value={item.danbooru_name} onCopy={handleCopy} />
+        <p className='m-1'>{item.danbooru_name}</p>
       </div>
       <div className='mt-3'>
-        <p>帰属</p>
-        <p>{item.copyrights}</p>
+        <TitleWithCopy label="帰属" value={item.copyrights} onCopy={handleCopy} />
+        <p className='m-1'>{item.copyrights}</p>
       </div>
-      <div className='whitespace-pre-wrap mt-3'>メモ:
-        <Button text="コピー" onClick={handleCopy} />
-        <div className='border-[1px] rounded border-gray p-2'>
+      <div className='whitespace-pre-wrap mt-3'>
+        <TitleWithCopy label="メモ" value={item.note} onCopy={handleCopy} />
+        <div className='border-[1px] rounded border-gray p-2 m-1'>
           {item.note}
         </div>
       </div>

67fe159badbcd8f04db8f16a9a614a780322c640
LittleRaisin <h_kiyomiya@msn.com>
2025-06-26 09:21:12 +0900

    レイアウト変更

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -4,21 +4,20 @@ import Home from './pages/Home';
 import SearchResult from './pages/SearchResult';
 import Edit from './pages/Edit';
 import FavoriteCategoryList from './pages/FavoriteCategoryList';
-import Header from './components/Header';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <HashRouter>
-      <Header />
-      <main className="p-4">
+      <Layout>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/search/:promptName' element={<SearchResult />} />
           <Route path='/create' element={<Edit />} /> 
           <Route path='/edit/:promptName' element={<Edit />} />
-          <Route path='/favorite/:category' element={<FavoriteCategoryList />} /> {/* 新しいルートを追加 */}
+          <Route path='/favorite/:category' element={<FavoriteCategoryList />} />
         </Routes>
-      </main>
+      </Layout>
     </HashRouter>
   );
 }
